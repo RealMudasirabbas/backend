64e8e68cda7e11905e688227f50d4eeca2bd3840
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-04-18 11:30:29 +0500

    complete user and video models schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import connectdb from "./db/connectDB.js";
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
 })
 
 connectdb()
+.then(() => {
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+})
+})
+.catch((err) => {
+
+    console.log("Database connection failed", err);
+})
 
